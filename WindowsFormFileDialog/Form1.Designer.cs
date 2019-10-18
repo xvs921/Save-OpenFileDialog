ac130edd6666fae8ae6121a2d7aaf9bf41085eb4
Petrik Lajos Szg <info@petrik.hu>
2019-10-18 12:57:10 +0200

    kesz

--- WindowsFormFileDialog/Form1.Designer.cs
+++ WindowsFormFileDialog/Form1.Designer.cs
@@ -28,13 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.tbBeir = new System.Windows.Forms.TextBox();
+            this.listSzovegek = new System.Windows.Forms.ListBox();
+            this.btnMent = new System.Windows.Forms.Button();
+            this.btnBetolt = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "TextFile(*.txt)|*.txt|All files(*.*)|*.*";
+            // 
+            // tbBeir
+            // 
+            this.tbBeir.Location = new System.Drawing.Point(12, 30);
+            this.tbBeir.Name = "tbBeir";
+            this.tbBeir.Size = new System.Drawing.Size(164, 20);
+            this.tbBeir.TabIndex = 0;
+            this.tbBeir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbBeir_KeyDown);
+            // 
+            // listSzovegek
+            // 
+            this.listSzovegek.FormattingEnabled = true;
+            this.listSzovegek.Location = new System.Drawing.Point(182, 30);
+            this.listSzovegek.Name = "listSzovegek";
+            this.listSzovegek.Size = new System.Drawing.Size(120, 394);
+            this.listSzovegek.TabIndex = 1;
+            // 
+            // btnMent
+            // 
+            this.btnMent.Location = new System.Drawing.Point(50, 66);
+            this.btnMent.Name = "btnMent";
+            this.btnMent.Size = new System.Drawing.Size(75, 23);
+            this.btnMent.TabIndex = 2;
+            this.btnMent.Text = "Mentés";
+            this.btnMent.UseVisualStyleBackColor = true;
+            this.btnMent.Click += new System.EventHandler(this.btnMent_Click);
+            // 
+            // btnBetolt
+            // 
+            this.btnBetolt.Location = new System.Drawing.Point(50, 107);
+            this.btnBetolt.Name = "btnBetolt";
+            this.btnBetolt.Size = new System.Drawing.Size(75, 23);
+            this.btnBetolt.TabIndex = 3;
+            this.btnBetolt.Text = "Betöltés";
+            this.btnBetolt.UseVisualStyleBackColor = true;
+            this.btnBetolt.Click += new System.EventHandler(this.btnBetolt_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "TextFile(*.txt)|*.txt|All files(*.*)|*.*";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBetolt);
+            this.Controls.Add(this.btnMent);
+            this.Controls.Add(this.listSzovegek);
+            this.Controls.Add(this.tbBeir);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox tbBeir;
+        private System.Windows.Forms.ListBox listSzovegek;
+        private System.Windows.Forms.Button btnMent;
+        private System.Windows.Forms.Button btnBetolt;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
     }
 }
 
--- WindowsFormFileDialog/Form1.cs
+++ WindowsFormFileDialog/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace WindowsFormFileDialog
 {
@@ -15,6 +16,57 @@ namespace WindowsFormFileDialog
         public Form1()
         {
             InitializeComponent();
+
+            saveFileDialog1.FileOk += (senderFile, eFile) =>
+            {
+                try
+                {
+                    string fileName = saveFileDialog1.FileName;
+                    File.WriteAllLines(fileName, listSzovegek.Items.Cast<string>().ToArray());
+
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Hiba! Nem sikerült a kiírás.");
+                }
+
+            };
+
+            openFileDialog1.FileOk += (sender, e) =>
+            {
+                try
+                {
+                    string[] sorok = File.ReadAllLines(openFileDialog1.FileName);
+                    listSzovegek.Items.Clear();
+                    foreach (var item in sorok)
+                    {
+                        listSzovegek.Items.Add(item);
+                    }
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("Hiba! Nem sikerült betöltés.");
+                }
+            };
+        }
+
+        private void tbBeir_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                listSzovegek.Items.Add(tbBeir.Text);
+                tbBeir.Text = "";
+            }
+        }
+
+        private void btnMent_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void btnBetolt_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
         }
     }
 }
